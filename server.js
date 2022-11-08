3464b910f02fb2b1c42502200945f70fc3778b75
roodhouse <rughjm@gmail.com>
2022-11-08 14:34:44 -0600

    more files

--- server.js
+++ server.js
@@ -3,15 +3,38 @@ const session        = require('express-session');
 const routes         = require('./controllers');
 const sequelize      = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize');
+const exhbs          = require('express-handlebars');
+const path           = require('path');
 
 const app  = express();
 const PORT = process.env.PORT || 3001;
+const hbs  = exhbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 })
+
+
+
+// Server or app.js
+// <Route path='/' component=Homepage>
+// <Route path='/login' component=Login>
+
+
+//Components or pages
+/*
+const Homepage() {
+    return (
+        <div> This is my HomePage</div>
+    )
+}
+*/
